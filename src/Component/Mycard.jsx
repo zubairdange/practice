524748bcbfb5965abea033ebf145866f038a5c6d
zubairdange <71386093+zubairdange@users.noreply.github.com>
2020-12-19 13:14:27 +0530

    update

--- src/Component/Mycard.jsx
+++ src/Component/Mycard.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { Card, Row, Col } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import img1 from "./Images/img1.jpg";
-import img2 from "./Images/img2.jpg";
-import img3 from "./Images/img3.jpg";
-import img4 from "./Images/img4.jpg";
+
 export const Mycard = ({ itemCurent }) => {
   return (
     <div>
