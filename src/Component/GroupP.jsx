61c8b39cc74252cba5225017a7aad1f5e7540795
zubairdange <71386093+zubairdange@users.noreply.github.com>
2021-01-04 21:18:36 +0530

    dontknow

--- src/Component/GroupP.jsx
+++ src/Component/GroupP.jsx
@@ -1,21 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Group } from "./Group";
-import { getAllGroup } from "./State/selecter/groups";
-export const GroupP = () => {
-  const selector = useSelector(getAllGroup);
-  const [groupData, setGroupData] = useState();
-  useEffect(() => {
-    setGroupData(selector);
-    console.log("hiiiiiiiiii", selector);
-  }, [selector]);
-  console.log("groupsdata=====>", groupData);
-  return (
-    <div>
-      {groupData &&
-        groupData.map((item) => {
-          return <Group itemCurent={item} />;
-        })}
-    </div>
-  );
-};
+import React, { Component } from 'react'
+import Chart from "chart.js";
+import classes from "./LineGraph.module.css";
+
+export default class LineGraph extends Component {
+    chartRef = React.createRef();
+    
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+        
+        new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["Jan", "Feb", "March"],
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: [86, 67, 91],
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    }
+    render() {
+        return (
+            <div className={classes.graphContainer}>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
